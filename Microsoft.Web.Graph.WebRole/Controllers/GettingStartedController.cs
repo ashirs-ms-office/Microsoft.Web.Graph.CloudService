e0dbc35e3511426137fe4da7b1a444639e7431ca
Ashirvad Sahu <ashirs@microsoft.com>
2016-10-13 13:31:51 -0700

    Added getting started controller and basic view

---
+++ Microsoft.Web.Graph.WebRole/Controllers/GettingStartedController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Web.Graph.WebRole.ViewModels;
+using Microsoft.Web.Portal.Common.Culture;
+using Microsoft.Web.Portal.Common.Logging;
+using Microsoft.Web.Portal.Common.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Microsoft.Web.Graph.WebRole.Controllers
+{
+    public class GettingStartedController : BaseController
+    {
+        private ICultureService _cultureService = null;
+        private ILogger _logger = null;
+        private ITelemetry _telemetry = null;
+        public GettingStartedController(ICultureService cultureService, ILogger logger, ITelemetry telemetry)
+        {
+            _cultureService = cultureService;
+            _logger = logger;
+            _telemetry = telemetry;
+        }
+
+        // GET: GettingStarted
+        public ActionResult Index()
+        {
+            ViewModelBase model = new ViewModelBase(HttpContext.ApplicationInstance.Context);
+            model.PageTitle = "Microsoft Graph - Getting Started";
+            return View(model);
+        }
+    }
+}
