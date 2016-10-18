2c080f526b66a5cd7b08e12eea73dab74700a56a
Ashirvad Sahu <ashirs@microsoft.com>
2016-10-18 14:20:50 -0700

    Added the code to read the text from resources for getting started-intro partial view

--- Microsoft.Web.Graph.WebRole/ViewModels/GettingStarted/Partials/IntroViewModel.cs
+++ Microsoft.Web.Graph.WebRole/ViewModels/GettingStarted/Partials/IntroViewModel.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Web.Graph.WebRole.ViewModels.GettingStarted.Partials
 {
     public class IntroPartialViewModel
     {
-        public string Title { get; set; } = "Microsoft Graph Quick Start";
-        public string Description { get; set; } = "This quick start will let you try out calls, register your app, and download a sample app that connects to data using Microsoft Graph. The sample app sends an email on a user's behalf. It will show you how to authenticate and make calls to Microsoft Graph using a school or work or a Microsoft account.";
+        public string Title { get; set; } = Resources.GettingStarted.Index.MS_GRAPH_QUICK_START;
+        public string Description { get; set; } = Resources.GettingStarted.Index.MS_GRAPH_QUICK_START_DESCRIPTION;
     }
 }
